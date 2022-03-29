2a7683e017964f47073ef48e5c54addd164a71ce
pam <take_asi@hotmail.com>
2022-03-29 13:53:31 -0500

    post api works

--- api/posts.js
+++ api/posts.js
@@ -1,26 +1,45 @@
-const express = require('express');
+const express = require("express");
 const postsRouter = express.Router();
 
-const {getAllPosts} = require ('../db');
-const { requireUser } = require('./utils');
+const { getAllPosts, createPost } = require("../db");
+const { requireUser } = require("./utils");
 
-postsRouter.post('/', requireUser, async (req, res, next) => {
-  res.send({ message: 'under construction' });
+postsRouter.use((req, res, next) => {
+  console.log("Request made to /posts.");
+
+  next();
+});
+postsRouter.post("/", requireUser, async (req, res, next) => {
+  const { title, content, tags = "" } = req.body;
+
+  const tagArr = tags.trim().split(/\s+/);
+  const postData = {};
+
+  // only send the tags if there are some to send
+  if (tagArr.length) {
+    postData.tags = tagArr;
+  }
+
+  try {
+    // add authorId, title, content to postData object
+    postData.authorId = req.user.id;
+    postData.title = title;
+    postData.content = content;
+    // const post = await createPost(postData);
+    const post = await createPost(postData);
+    // this will create the post and the tags for us
+    // if the post comes back, res.send({ post });
+    res.send({post})
+    // otherwise, next an appropriate error object
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
 });
 
-
-postsRouter.use((req, res, next) =>{
-    console.log("Request made to /posts.");
-
-    next();
-})
-
-postsRouter.get('/', async(req, res) =>{
-    const posts = await getAllPosts();
-    res.send({
-        posts
-    });
-
-    
+postsRouter.get("/", async (req, res) => {
+  const posts = await getAllPosts();
+  res.send({
+    posts,
+  });
 });
 module.exports = postsRouter;
