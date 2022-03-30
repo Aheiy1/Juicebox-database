baa1fa378ec9007922f6d5e34455f78b92f71a4b
pam <take_asi@hotmail.com>
2022-03-30 09:51:45 -0500

    get tags name

--- api/tags.js
+++ api/tags.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const tagsRouter = express.Router();
 
-const { getAllTags } = require("../db");
+const { getAllTags, getPostsByTagName } = require("../db");
 
 tagsRouter.use((req, res, next) => {
   console.log("Request to tags");
 
   next();
+});
+
+tagsRouter.get("/:tagName/posts", async (req, res, next) => {
+  // read the tagname from the params
+  const { tagName } = req.params;
+
+  try {
+    // use our method to get posts by tag name from the db
+    const posts = await getPostsByTagName(tagName);
+
+    // send out an object to the client { posts: // the posts }
+    res.send({ posts });
+  } catch ({ name, message }) {
+    // forward the name and message to the error handler
+    next({ name, message });
+  }
 });
 
 tagsRouter.get("/", async (req, res) => {
@@ -15,4 +31,5 @@ tagsRouter.get("/", async (req, res) => {
     tags,
   });
 });
+
 module.exports = tagsRouter;
